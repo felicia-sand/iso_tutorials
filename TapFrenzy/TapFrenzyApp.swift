b50698d57d5f2244f825fc3c9d59eba2dc9f9834
Student1 <student1@NIBMADMIN02s-iMac.local>
2026-06-10 12:44:54 +0530

    feat: initial project setup and layout foundation

---
+++ TapFrenzy/TapFrenzyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapFrenzyApp.swift
+//  TapFrenzy
+//
+//  Created by Student1 on 2026-06-10.
+//
+
+import SwiftUI
+
+@main
+struct TapFrenzyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
